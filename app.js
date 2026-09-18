de91a278e665012877b73993e08179b36d47e4e2
Ajay Soni <officialprofessionalmail@gmail.com>
2026-09-18 05:52:24 +0530

    Finalize icon balance and image viewer styling

--- app.js
+++ app.js
@@ -56,7 +56,7 @@
     document.head.appendChild(link);
   };
 
-  loadStylesheet('portfolio-enhancements', '/portfolio-enhancements.css?v=20260918-5');
+  loadStylesheet('portfolio-enhancements', '/portfolio-enhancements.css?v=20260918-6');
   loadStylesheet('desktop-responsive', '/desktop-responsive.css?v=20260918-2');
 
   const canonicalUrl = 'https://ajaysonidev.vercel.app/';
